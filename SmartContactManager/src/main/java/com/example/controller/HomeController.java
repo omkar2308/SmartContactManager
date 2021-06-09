c335e41368de86e82dc68aed5589408d24c33493
OMKAR HAJARE <OMKAR HAJARE@LAPTOP-D40P45RS>
2021-06-09 18:49:45 +0530

    save user data in db

--- SmartContactManager/src/main/java/com/example/controller/HomeController.java
+++ SmartContactManager/src/main/java/com/example/controller/HomeController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.dao.UserRepository;
@@ -14,7 +16,7 @@ import com.example.entities.User;
 
 @Controller
 public class HomeController {
-	
+
 //	@Autowired
 //	private UserRepository userRepository;
 //	
@@ -32,29 +34,49 @@ public class HomeController {
 //		userRepository.save(user);
 //		return "working";
 //	}
+
+	@Autowired
+	private UserRepository userRepository;
 	
-	@RequestMapping( "/" )
+	@RequestMapping("/")
 	public String home(Model m) {
-		
-		m.addAttribute("title","Home-Smart Contact");
+
+		m.addAttribute("title", "Home-Smart Contact");
 		return "home";
 	}
-	
-	@RequestMapping( "/about" )
+
+	@RequestMapping("/about")
 	public String about(Model m) {
-		
-		m.addAttribute("title","About-Smart Contact");
+
+		m.addAttribute("title", "About-Smart Contact");
 		return "about";
 	}
-	@RequestMapping( "/signup" )
+
+	@RequestMapping("/signup")
 	public String signup(Model m) {
+
+		m.addAttribute("title", "Register-Smart Contact");
+		m.addAttribute("user", new User());
+		return "signup";
+	}
+
+//	handler for registering user
+	@RequestMapping(value = "/do_register", method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("user") User user,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model m) {
+		if(!agreement) {
+			System.out.println("you have not aggred terms and conditions");
+		}
 		
-		m.addAttribute("title","Register-Smart Contact");
+		user.setRole("ROAL_USER");
+		user.setEnabled(true);
+		user.setImageUrl("default.png");
+		
+		User result = this.userRepository.save(user);
+		m.addAttribute("user",result);
+		
+		System.out.println("agreement " + agreement);
+		System.out.println("user" + user);
 		return "signup";
 	}
 }
-
-
-
-
-

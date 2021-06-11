672421c1fe60b38963f586f456857781b78458ab
OMKAR HAJARE <OMKAR HAJARE@LAPTOP-D40P45RS>
2021-06-11 21:13:13 +0530

    47 impl of userdetail and userservice

---
+++ SmartContactManager/src/main/java/com/example/config/CustamUserDetails.java
@@ -0,0 +1,63 @@
+package com.example.config;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.entities.User;
+
+public class CustamUserDetails implements UserDetails {
+
+	private User user;
+
+	public CustamUserDetails(User user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole());
+		
+		return List.of(simpleGrantedAuthority);
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
